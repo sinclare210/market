bb6dca98e1a9cb0d61be6815b8750cb62ace31d0
Sinclair <sinclairolajuwon@gmail.com>
2024-10-21 13:58:56 -0500

    almost

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
 import { useState } from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Product from './pages/Product/Product';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import "./app.scss"
 
+const Layout = () =>{
 
+  return(
+    <div className='app'>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/products/:id",
+    element: <Products/>,
+  },
+  {
+    path: "/product/:id",
+    element: <Product/>,
+  },
+    ]
+  }
+]);
 
 function App() {
 
 
   return (
     <>
-     
+    <RouterProvider router={router} />
     </>
   )
 }
