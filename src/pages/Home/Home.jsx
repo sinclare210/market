2e64d6dd657a6aa6f70f8f553a3e4dc8079c63df
Sinclair <sinclairolajuwon@gmail.com>
2024-10-21 16:00:14 -0500

    navbar corrected

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Slider from '../../components/Slider/Slider'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import Categories from '../../components/Categories/Categories'
+import Contact from '../../components/Contact/Contact'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
         <FeaturedProducts type="featured"/>
              <Categories/>
       <FeaturedProducts type="trending"/>
+      <Contact/>
     </div>
   )
 }
