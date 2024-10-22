92660fdcb2fbfeab49787a00578cc32bf384df7b
Sinclair <sinclairolajuwon@gmail.com>
2024-10-22 05:54:24 -0500

    product

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -20,3 +20,51 @@
 // };
 
 // export default List;
+
+import React from "react";
+import "./List.scss";
+import Card from "../Card/Card";
+// import useFetch from "../../hooks/useFetch";
+
+const List = ({ subCats, maxPrice, sort, catId }) => {
+
+  const data = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Elegant Red Dress",
+    price: 59.99,
+    oldPrice: 79.99,
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/27333760/pexels-photo-27333760/free-photo-of-portrait-of-a-man-smiling.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    title: "Men's Casual Jacket",
+    price: 89.99,
+    oldPrice: 119.99,
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/28555848/pexels-photo-28555848/free-photo-of-woman-holding-turkish-coffee-and-delight-in-istanbul.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    title: "Stylish Sunglasses",
+    price: 25.99,
+    oldPrice: 35.99,
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/4462786/pexels-photo-4462786.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Leather Handbag",
+    price: 149.99,
+    oldPrice: 199.99,
+  },
+];
+ 
+
+  return (
+    <div className="list">
+      {data?.map((item) => <Card item={item} key={item.id} />)}
+    </div>
+  );
+};
+
+export default List;
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -97,10 +97,46 @@
 import React from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import List from "../../components/List/List";
+// import useFetch from "../../hooks/useFetch";
 import "./Products.scss";
 
-const Products = () => {
+const data = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Elegant Red Dress",
+    price: 59.99,
+    oldPrice: 79.99,
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/27333760/pexels-photo-27333760/free-photo-of-portrait-of-a-man-smiling.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    title: "Men's Casual Jacket",
+    price: 89.99,
+    oldPrice: 119.99,
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/28555848/pexels-photo-28555848/free-photo-of-woman-holding-turkish-coffee-and-delight-in-istanbul.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    title: "Stylish Sunglasses",
+    price: 25.99,
+    oldPrice: 35.99,
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/4462786/pexels-photo-4462786.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Leather Handbag",
+    price: 149.99,
+    oldPrice: 199.99,
+  },
+];
+ 
 
+const Products = () => {
+   const catId = parseInt(useParams().id);
+   const [maxPrice, setMaxPrice] = useState(1000);
+    const [sort, setSort] = useState(null);
 
   return (
     <div className="products">
@@ -113,7 +149,7 @@ const Products = () => {
                 type="checkbox"
                 id={1}
                 value={1}
-                onChange={handleChange}
+               
               />
               <label htmlFor={1}>{1}</label>
             </div>
@@ -162,7 +198,7 @@ const Products = () => {
           src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
           alt=""
         />
-        <List catId={catId} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats}/>
+        <List catId={catId} maxPrice={maxPrice} />
       </div>
     </div>
   );
